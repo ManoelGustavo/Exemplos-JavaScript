b285150b6a31468b1a65e01371d0a37bb0019f0e
ManoelGustavo <50558011+ManoelGustavo@users.noreply.github.com>
2019-07-17 04:49:54 -0300

    Exemplos Simples de JavaScript
    funções e operadores logicos

---
+++ Exemplo01.js
@@ -0,0 +1,11 @@
+
+
+function func(x , y){//escopo de bloco , função
+    var x = 10;
+    var y = 10;
+    return x + y;
+}
+alert(func());
+console.log(func())
+
+
---
+++ Exemplo02.js
@@ -0,0 +1,15 @@
+var x = 5;
+var y = 10;
+
+function or() {
+    return x > 10 
+}
+function and() {
+    return x < 10 && y>10;
+}
+function not() {
+    return ! x == 10;
+}
+console.log("função retornada  OR: " ,or());
+console.log("funçao retornada AND" , and());
+console.log("funçao retornada NOT" , not());
